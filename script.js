e951c69d5db4ac579fe3358b34a3b8af161208b9
Coding's Time <87648986+codingstime@users.noreply.github.com>
2021-08-14 19:47:06 +0700

    create control buttons and indicators

--- script.js
+++ script.js
@@ -4,8 +4,20 @@
  * Github Profile    : https://github.com/codingstime
  */
 
+let onSlide = false;
+
 window.addEventListener("load", () => {
    autoSlide();
+
+   const dots = document.querySelectorAll(".carousel_dot");
+   for (let i = 0; i < dots.length; i++) {
+      dots[i].addEventListener("click", () => slide(i));
+   }
+
+   const buttonPrev = document.querySelector(".carousel_button__prev");
+   const buttonNext = document.querySelector(".carousel_button__next");
+   buttonPrev.addEventListener("click", () => slide(getItemActiveIndex() - 1));
+   buttonNext.addEventListener("click", () => slide(getItemActiveIndex() + 1));
 })
 
 function autoSlide() {
@@ -15,30 +27,55 @@ function autoSlide() {
 }
 
 function slide(toIndex) {
+   if (onSlide)
+      return;
+   onSlide = true;
+
    const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
    const itemActive = document.querySelector(".carousel_item__active");
+   const itemActiveIndex = itemsArray.indexOf(itemActive);
+   let newItemActive = null;
 
-   // check if toIndex exceeds the number of carousel items
-   if (toIndex >= itemsArray.length) {
-      toIndex = 0;
+   if (toIndex > itemActiveIndex) {
+      // check if toIndex exceeds the number of carousel items
+      if (toIndex >= itemsArray.length) {
+         toIndex = 0;
+      }
+
+      newItemActive = itemsArray[toIndex];
+
+      // start transition
+      newItemActive.classList.add("carousel_item__pos_next");
+      setTimeout(() => {
+         newItemActive.classList.add("carousel_item__next");
+         itemActive.classList.add("carousel_item__next");
+      }, 20);
+   } else {
+      // check if toIndex exceeds the number of carousel items
+      if (toIndex < 0) {
+         toIndex = itemsArray.length - 1;
+      }
+
+      newItemActive = itemsArray[toIndex];
+
+      // start transition
+      newItemActive.classList.add("carousel_item__pos_prev");
+      setTimeout(() => {
+         newItemActive.classList.add("carousel_item__prev");
+         itemActive.classList.add("carousel_item__prev");
+      }, 20);
    }
-
-   const newItemActive = itemsArray[toIndex];
-
-   // start transition
-   newItemActive.classList.add("carousel_item__pos_next");
-   setTimeout(() => {
-      newItemActive.classList.add("carousel_item__next");
-      itemActive.classList.add("carousel_item__next");
-   }, 20);
 
    // remove all transition class and switch active class
    newItemActive.addEventListener("transitionend", () => {
       itemActive.className = "carousel_item";
       newItemActive.className = "carousel_item carousel_item__active";
+      onSlide = false;
    }, {
       once: true
    });
+
+   slideIndicator(toIndex);
 }
 
 function getItemActiveIndex() {
@@ -46,4 +83,13 @@ function getItemActiveIndex() {
    const itemActive = document.querySelector(".carousel_item__active");
    const itemActiveIndex = itemsArray.indexOf(itemActive);
    return itemActiveIndex;
+}
+
+function slideIndicator(toIndex) {
+   const dots = document.querySelectorAll(".carousel_dot");
+   const dotActive = document.querySelector(".carousel_dot__active");
+   const newDotActive = dots[toIndex];
+
+   dotActive.classList.remove("carousel_dot__active");
+   newDotActive.classList.add("carousel_dot__active");
 }
